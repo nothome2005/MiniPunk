8c4ab3f4ea9f9c9c300b4cdadedb4efa91279096
Fit4a <andrey.maslov2005@gmail.com>
2025-05-14 23:07:58 +0300

    Первый коммит: добавлены исходники, ресурсы и include

---
+++ src/main.cpp
@@ -0,0 +1,53 @@
+/*
+Raylib example file.
+This is an example main file for a simple raylib project.
+Use this as a starting point or replace it with your code.
+
+by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit https://creativecommons.org/publicdomain/zero/1.0/
+
+*/
+
+#include "raylib.h"
+
+#include "resource_dir.h"	// utility header for SearchAndSetResourceDir
+
+int main ()
+{
+	// Tell the window to use vsync and work on high DPI displays
+	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
+
+	// Create the window and OpenGL context
+	InitWindow(1024, 1024, "MiniPunk");
+
+	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
+	SearchAndSetResourceDir("resources");
+
+	// Load a texture from the resources directory
+	Texture back_ = LoadTexture("background.png");
+	
+	// game loop
+	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
+	{
+		// drawing
+		BeginDrawing();
+
+		// Setup the back buffer for drawing (clear color and depth buffers)
+		ClearBackground(BLACK);
+
+		// draw some text using the default font
+
+		// draw our texture to the screen
+		DrawTexture(back_, 0, 0, WHITE);
+		
+		// end the frame and get ready for the next one  (display frame, poll input, etc...)
+		EndDrawing();
+	}
+
+	// cleanup
+	// unload our texture so it can be cleaned up
+	UnloadTexture(back_);
+
+	// destroy the window and cleanup the OpenGL context
+	CloseWindow();
+	return 0;
+}
